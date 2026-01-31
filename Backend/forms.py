c71d318babcb88d51fc226b277542b81862c7bf7
Fanera <egor.powwer@gmail.com>
2026-02-01 02:17:47 +1000

    формы и схемы бэкенда

---
+++ Backend/forms.py
@@ -0,0 +1,128 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, PasswordField, SelectField, SubmitField, DateTimeField, IntegerField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from datetime import datetime
+from models import UserRole, ProjectRole, TaskPriority, TaskCategory, TaskStatus, Color
+
+def validate_deadline(form, field):
+    if field.data and field.data < datetime.now():
+        raise ValidationError('Дедлайн не может быть в прошлом')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[
+        DataRequired(),
+        Length(min=3, max=64, message='Имя пользователя должно быть от 3 до 64 символов')
+    ])
+    email = StringField('Email', validators=[DataRequired(), Email(message='Некорректный email')])
+    password = PasswordField('Пароль', validators=[
+        DataRequired(),
+        Length(min=6, max=256, message="Пароль от 6 до 256 символов")
+    ])
+    confirm_password = PasswordField('Подтвердите пароль', validators=[
+        DataRequired(),
+        EqualTo('password', message='Пароли не совпадают')
+    ])
+    submit = SubmitField('Зарегистрироваться')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email(message='Некорректный email')])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
+
+class ProjectForm(FlaskForm):
+    name = StringField('Название проекта', validators=[
+        DataRequired(),
+        Length(max=128, message='Название не должно превышать 128 символов')
+    ])
+    description = TextAreaField('Описание')
+    color = SelectField('Цвет', choices=[
+        (Color.BLACK.value, 'Черный'),
+        (Color.RED.value, 'Красный'),
+        (Color.ORANGE.value, 'Оранжевый'),
+        (Color.YELLOW.value, 'Желтый'),
+        (Color.GREEN.value, 'Зеленый'),
+        (Color.CIAN.value, 'Голубой'),
+        (Color.BLUE.value, 'Синий'),
+        (Color.VIOLET.value, 'Фиолетовый'),
+        (Color.WHITE.value, 'Белый')
+    ], default=Color.WHITE.value)
+    submit = SubmitField('Создать проект')
+
+class ProjectMemberFrom(FlaskForm):
+    user_id = IntegerField('ID пользователя', validators=[DataRequired()])
+    role = SelectField('Роль в проекте', choices=[
+        (ProjectRole.OWNER.value, 'Владелец'),
+        (ProjectRole.EDITOR.value, 'Редактор'),
+        (ProjectRole.VIEWER.value, 'Наблюдатель')
+    ], default=ProjectRole.VIEWER.value)
+    submit = SubmitField('Установить роль')
+
+class TaskForm(FlaskForm):
+    title = StringField('Название задачи', validators=[
+        DataRequired(),
+        Length(max=128, message='Название не должно превышать 128 символов')
+    ])
+    description = TextAreaField('Описание')
+    priority = SelectField('Приоритет', choices=[
+        (TaskPriority.NONE.value, 'Без приоритета'),
+        (TaskPriority.LOW.value, 'Низкий'),
+        (TaskPriority.MEDIUM.value, 'Средний'),
+        (TaskPriority.HIGH.value, 'Высокий'),
+        (TaskPriority.CRITICAL.value, 'Критический')
+    ], default=TaskPriority.NONE.value)
+    category = SelectField('Категория', choices=[
+        (TaskCategory.NONE.value, 'Без категории'),
+        (TaskCategory.BUG.value, 'Ошибка'),
+        (TaskCategory.FEATURE.value, 'Функция'),
+        (TaskCategory.IMPROVEMENT.value, 'Улучшение'),
+        (TaskCategory.DOCUMENTATION.value, 'Документация')
+    ], default=TaskCategory.NONE.value)
+    status = SelectField('Статус', choices=[
+        (TaskStatus.NONE.value, 'Не задан'),
+        (TaskStatus.TODO.value, 'К выполнению'),
+        (TaskStatus.INPROGRESS.value, 'В работе'),
+        (TaskStatus.REVIEW.value, 'На проверке'),
+        (TaskStatus.DONE.value, 'Выполнено')
+    ], default=TaskStatus.NONE.value)
+    deadline_date = DateTimeField('Дедлайн', format='%Y-%m-%d %H:%M', validators=[validate_deadline])
+    project_id = IntegerField('ID проекта', validators=[DataRequired()])
+    parent_id = IntegerField('ID родительской задачи')
+    submit = SubmitField("Добавить задачу")
+
+class TaskFilterForm(FlaskForm):
+    project_id = IntegerField('Проект')
+    priority = SelectField('Приоритет', choices=[
+        ('', 'Все'),
+        (TaskPriority.NONE.value, 'Без приоритета'),
+        (TaskPriority.LOW.value, 'Низкий'),
+        (TaskPriority.MEDIUM.value, 'Средний'),
+        (TaskPriority.HIGH.value, 'Высокий'),
+        (TaskPriority.CRITICAL.value, 'Критический')
+    ])
+    category = SelectField('Категория', choices=[
+        ('', 'Все'),
+        (TaskCategory.NONE.value, 'Без категории'),
+        (TaskCategory.BUG.value, 'Ошибка'),
+        (TaskCategory.FEATURE.value, 'Функция'),
+        (TaskCategory.IMPROVEMENT.value, 'Улучшение'),
+        (TaskCategory.DOCUMENTATION.value, 'Документация')
+    ])
+    status = SelectField('Статус', choices=[
+        ('', 'Все'),
+        (TaskStatus.NONE.value, 'Не задан'),
+        (TaskStatus.TODO.value, 'К выполнению'),
+        (TaskStatus.INPROGRESS.value, 'В работе'),
+        (TaskStatus.REVIEW.value, 'На проверке'),
+        (TaskStatus.DONE.value, 'Выполнено')
+    ])
+    assignee_id = IntegerField('Исполнитель')
+    search = StringField('Поиск')
+    submit = SubmitField('Поиск')
+
+class CommentForm(FlaskForm):
+    text_comment = TextAreaField('Комментарий', validators=[
+        DataRequired(),
+        Length(min=1, message='Комментарий не может быть пустым')
+    ])
+    task_id = IntegerField('ID задачи', validators=[DataRequired()])
+    submit = SubmitField("Оставить комментарий")
---
+++ Backend/schemas.py
@@ -0,0 +1,104 @@
+from marshmallow import Schema, fields, validate, validates, ValidationError
+from datetime import datetime
+from models import UserRole, ProjectRole, TaskPriority, TaskCategory, TaskStatus, Color
+
+USER_ROLES = [role.value for role in UserRole]
+PROJECT_ROLES = [role.value for role in ProjectRole]
+TASK_PRIORITIES = [priority.value for priority in TaskPriority]
+TASK_CATEGORIES = [category.value for category in TaskCategory]
+TASK_STATUSES = [status.value for status in TaskStatus]
+COLORS = [color.value for color in Color]
+
+def validate_deadline_not_past(value):
+    if value == None:
+        raise ValidationError('Отсутствует дедлайн!')
+    if value < datetime.now():
+        raise ValidationError('Дедлайн не может быть в прошлом!')
+    return value
+
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    username = fields.Str(required=True, validate=validate.Length(min=3, max=64))
+    email = fields.Email(required=True)
+    password = fields.Str(required=True, load_only=True, validate=validate.Length(min=6, max=256))
+    role = fields.Str(required=True, validate=validate.OneOf(USER_ROLES))
+    confirm_password = fields.Str(load_only=True, required=False)
+
+class ProjectSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True, validate=validate.Length(max=128))
+    description = fields.Str(allow_none=True)
+    color = fields.Str(validate=validate.OneOf(COLORS))
+    owner = fields.Int(allow_none=True)
+    creation_date = fields.DateTime(dump_only=True)
+
+class TaskSchema(Schema):
+    id = fields.Int(dump_only=True)
+    title = fields.Str(required=True, validate=validate.Length(max=128))
+    description = fields.Str(allow_none=True)
+    priority = fields.Str(validate=validate.OneOf(TASK_PRIORITIES))
+    category = fields.Str(validate=validate.OneOf(TASK_CATEGORIES))
+    status = fields.Str(validate=validate.OneOf(TASK_STATUSES))
+    creation_date = fields.DateTime(dump_only=True)
+    deadline_date = fields.DateTime(allow_none=True)
+    project_id = fields.Int(required=True)
+    parent_id = fields.Int(allow_none=True)
+    assignee_ids = fields.List(fields.Int(), load_only=True, required=False)
+
+    @validates('deadline_date')
+    def validate_deadline(self, value):
+        return validate_deadline_not_past(value)
+
+class CommentSchema(Schema):
+    id = fields.Int(dump_only=True)
+    text_comment = fields.Str(required=True, validate=validate.Length(min=1))
+    creation_date = fields.DateTime(dump_only=True)
+    task_id = fields.Int(required=True)
+    author_id = fields.Int(dump_only=True)
+
+class ProjectMemberSchema(Schema):
+    user_id = fields.Int(required=True)
+    role = fields.Str(required=True, validate=validate.OneOf(PROJECT_ROLES))
+
+class TaskAssigneeSchema(Schema):
+    user_ids = fields.List(fields.Int(), required=True)
+
+class LoginSchema(Schema):
+    email = fields.Email(required=True)
+    password = fields.Str(required=True)
+
+class RegistrationSchema(Schema):
+    username = fields.Str(required=True, validate=validate.Length(min=3, max=64))
+    email = fields.Email(required=True)
+    password = fields.Str(required=True, validate=validate.Length(min=6, max=256))
+    confirm_password = fields.Str(required=True)
+    role = fields.Str(required=True, validate=validate.OneOf(USER_ROLES))
+
+    @validates('confirm_password')
+    def validate_passwords(self, value, **kwargs):
+        data = kwargs.get('data', {})
+        if 'password' in data and value != data['password']:
+            raise ValidationError('Пароли не совпадают')
+
+class TaskFilterSchema(Schema):
+    project_id = fields.Int(allow_none=True)
+    priority = fields.Str(allow_none=True, validate=validate.OneOf(TASK_PRIORITIES))
+    category = fields.Str(allow_none=True, validate=validate.OneOf(TASK_CATEGORIES))
+    status = fields.Str(allow_none=True, validate=validate.OneOf(TASK_STATUSES))
+    assignee_id = fields.Int(allow_none=True)
+    search = fields.Str(allow_none=True)
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+project_schema = ProjectSchema()
+projects_schema = ProjectSchema(many=True)
+task_schema = TaskSchema()
+tasks_schema = TaskSchema(many=True)
+comment_schema = CommentSchema()
+comments_schema = CommentSchema(many=True)
+login_schema = LoginSchema()
+registration_schema = RegistrationSchema()
+project_member_schema = ProjectMemberSchema()
+task_assignee_schema = TaskAssigneeSchema()
+task_filter_schema = TaskFilterSchema()
